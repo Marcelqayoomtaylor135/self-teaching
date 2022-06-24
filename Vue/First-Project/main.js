5a3226b239d48d09380e836fd2bb31a8859f1fba
Marcel Qayoom-Taylor <marcelqayoomtaylor@gmail.com>
2022-06-24 16:16:29 +1000

    Conditional Rendering, List Rendering, Event Handling

--- Vue/First-Project/main.js
+++ Vue/First-Project/main.js
@@ -5,7 +5,36 @@ var app = new Vue({
         image: "vmSocks-green-onWhite.jpg",
         product: 'Socks',
         description: 'The comfiest socks you will ever wear in your life!',
+        inventory: 6,
+        onSale: true,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green",
+                variantImage: "vmSocks-green-onWhite.jpg"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue",
+                variantImage: "vmSocks-blue-onWhite.jpg",
+            }
+        ],
+        sizes: ["small, medium, large"],
+        cart: 0,
+    },
+    methods: {
+        addToCart(){
+            this.cart += 1
+        },
+        removeFromCart(){
+            if (this.cart >= 1){
+                this.cart -= 1
+            }
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        },
         
-
     }
 })
