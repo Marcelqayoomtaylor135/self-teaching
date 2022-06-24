d8d21759771e7cd93714a398f002d18a34025cc6
Marcel Qayoom-Taylor <marcelqayoomtaylor@gmail.com>
2022-06-24 18:03:23 +1000

    Components and Communicating Events

--- Vue/First-Project/main.js
+++ Vue/First-Project/main.js
@@ -1,60 +1,135 @@
+Vue.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true,
+        }
+    },
+    template: `
+     <div class="product">
+          
+        <div class="product-image">
+          <img :src="image" />
+        </div>
+  
+        <div class="product-info">
+            <h1>{{ product }}</h1>
+            <p v-if="inStock">In Stock</p>
+            <p v-else>Out of Stock</p>
+            <p>Shipping: {{ shipping }}</p>
+            <productDetails :details="details"></productDetails>   
+  
+            <div class="color-box"
+                 v-for="(variant, index) in variants" 
+                 :key="variant.variantId"
+                 :style="{ backgroundColor: variant.variantColor }"
+                 @mouseover="updateProduct(index)"
+                 >
+            </div> 
+  
+            <button v-on:click="addToCart" 
+              :disabled="!inStock"
+              :class="{ disabledButton: !inStock }"
+              >
+            Add to cart
+            </button>
+
+            <button v-on:click="removeFromCart" 
+            :disabled="!inStock"
+            :class="{ disabledButton: !inStock }"
+            >
+            Remove from cart
+            </button>
+  
+
+  
+         </div>  
+      
+      </div>
+     `,
+    data() {
+      return {
+          product: 'Socks',
+          brand: 'Vue Mastery',
+          selectedVariant: 0,
+          details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+          variants: [
+            {
+              variantId: 2234,
+              variantColor: 'green',
+              variantImage:  'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+              variantQuantity: 10     
+            },
+            {
+              variantId: 2235,
+              variantColor: 'blue',
+              variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg',
+              variantQuantity: 0     
+            }
+          ],
+          cart: 0
+      }
+    },
+      methods: {
+        addToCart: function() {
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId)
+        },
+        removeFromCart(){
+            this.$emit('remove-from-cart', this.variants[this.selectedVariant].variantId)
+        },
+        updateProduct: function(index) {  
+            this.selectedVariant = index
+        }
+      },
+      computed: {
+          title() {
+              return this.brand + ' ' + this.product  
+          },
+          image(){
+              return this.variants[this.selectedVariant].variantImage
+          },
+          inStock(){
+              return this.variants[this.selectedVariant].variantQuantity
+          },
+          shipping() {
+            if (this.premium) {
+              return "Free"
+            }
+              return 2.99
+          }
+      }
+  })
+
+Vue.component('productDetails', {
+    props: {
+        details: {
+            type: Array,
+            required: true,
+        }
+    },
+    template: `
+        <ul>
+            <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+    `
+}) 
+
 var app = new Vue({
     el: '#app',
     data: {
-        prevPage: "https://www.google.com/",
-        selectedVariant: 0,
-        brand: 'Vue Mastery',
-        product: 'Socks',
-        description: 'The comfiest socks you will ever wear in your life!',
-        inventory: 6,
-        onSale: true,
-        details: ["80% cotton", "20% polyester", "Gender-neutral"],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: "green",
-                variantQuantity: 10,
-                variantImage: "vmSocks-green-onWhite.jpg",
-            },
-            {
-                variantId: 2235,
-                variantColor: "blue",
-                variantQuantity: 0,
-                variantImage: "vmSocks-blue-onWhite.jpg",
-            }
-        ],
-        sizes: ["small, medium, large"],
-        cart: 0,
+        premium: false,
+        cart: [],
     },
     methods: {
-        addToCart(){
-            this.cart += 1
+        updateCart(id) {
+            this.cart.push(id)
         },
-        removeFromCart(){
-            if (this.cart >= 1){
-                this.cart -= 1
+        removeFromCart(id){
+            for (let i = this.cart.length-1; i >= 0; i--){
+                if (this.cart[i] === id){
+                    this.cart.splice(i,1)
+                }
             }
-        },
-        updateProduct(index) {
-            this.selectedVariant = index
-        },
-    },
-    computed: {
-        title() {
-            return this.brand + " " + this.product
-        },
-        image() {
-            return this.variants[this.selectedVariant].variantImage
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].variantQuantity
-        },
-        sale(){
-            if (this.onSale){
-                return this.brand + ' ' + this.product + ' ' + 'are on sale!'
-            } 
-            
-            return this.brand + ' ' + this.product + ' ' + 'are not on sale!'
         }
     }
 })
