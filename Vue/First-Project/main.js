89bae0cee8fcbc6d12c8dee2e5b8ea12a84f889c
Marcel Qayoom-Taylor <marcelqayoomtaylor@gmail.com>
2022-06-24 15:27:46 +1000

    Instantiation and Attribute Binding

---
+++ Vue/First-Project/main.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        prevPage: "https://www.google.com/",
+        image: "vmSocks-green-onWhite.jpg",
+        product: 'Socks',
+        description: 'The comfiest socks you will ever wear in your life!',
+        
+
+    }
+})
